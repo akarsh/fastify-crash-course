5ccc954d179a553990dbe59d35ff5419a4105d27
Akarsh Seggemu <sakarshkumar@gmail.com>
2025-04-12 18:25:01 +0200

    Update dependencies and migrate to @fastify packages

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const fastify = require('fastify')({ logger: true })
-fastify.register(require('fastify-swagger'), {
-  exposeRoute: true,
-  routePrefix: '/docs',
+fastify.register(require('@fastify/swagger'), {
   swagger: {
     info: { title: 'fastify-api' },
   },
+})
+fastify.register(require('@fastify/swagger-ui'), {
+  exposeRoute: true,
+  routePrefix: '/docs',
 })
 fastify.register(require('./routes/items'))
 
@@ -12,7 +14,7 @@ const PORT = 5000
 
 const start = async () => {
   try {
-    await fastify.listen(PORT)
+    await fastify.listen({ port: PORT })
   } catch (error) {
     fastify.log.error(error)
     process.exit(1)
